d35b837edcc47a024a5214e51b4aea3c3ff50e10
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-07-02 16:26:14 +0800

    feat: 导航部分

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,9 +1,24 @@
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import styles from './index.module.scss';
+import { navs } from './config';
 
 const Footer: NextPage = () => {
+  const { pathname } = useRouter();
+
   return (
-    <div>
-      <p>导航</p>
+    <div className={styles.navbar}>
+      <section className={styles.logArea}>Blog-X</section>
+      <section className={styles.linkArea}>
+        {navs?.map((nav) => (
+          <Link key={nav?.label} href={nav?.value}>
+            <a className={pathname === nav?.value ? styles.active : ''}>
+              {nav?.label}
+            </a>
+          </Link>
+        ))}
+      </section>
     </div>
   );
 };
