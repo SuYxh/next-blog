011d67de34d451f777a2675576c4c7e36a62d0b9
yangxinhao <yangxinhao@zhuanzhuan.com>
2023-07-02 16:48:25 +0800

    feat: 登陆框结构和样式

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,4 +1,6 @@
 import styles from './index.module.scss';
+import { useState } from 'react';
+import CountDown from 'components/CountDown';
 
 interface IProps {
   isShow: boolean;
@@ -7,8 +9,87 @@ interface IProps {
 
 const Login = (props: IProps) => {
   const { isShow = false } = props;
+  const [isShowVerifyCode, setIsShowVerifyCode] = useState(false);
+  const [form, setForm] = useState({
+    phone: '',
+    verify: '',
+  });
 
-  return isShow ? <div className={styles.loginArea}></div> : null;
+  const handleClose = () => {
+    console.log('handleClose');
+  };
+
+  const handleFormChange = () => {
+    console.log('handleFormChange');
+  };
+
+  const handleGetVerifyCode = () => {
+    console.log('handleGetVerifyCode');
+  };
+
+  const handleCountDownEnd = () => {
+    console.log('handleCountDownEnd');
+  };
+
+  const handleLogin = () => {
+    console.log('handleLogin');
+  };
+
+  const handleOAuthGithub = () => {
+    console.log('handleOAuthGithub');
+  };
+
+  return isShow ? (
+    <div className={styles.loginArea}>
+      <div className={styles.loginBox}>
+        <div className={styles.loginTitle}>
+          <div>手机号登录</div>
+          <div className={styles.close} onClick={handleClose}>
+            x
+          </div>
+        </div>
+        <input
+          name="phone"
+          type="text"
+          placeholder="请输入手机号"
+          value={form.phone}
+          onChange={handleFormChange}
+        />
+        <div className={styles.verifyCodeArea}>
+          <input
+            name="verify"
+            type="text"
+            placeholder="请输入验证码"
+            value={form.verify}
+            onChange={handleFormChange}
+          />
+          <span className={styles.verifyCode} onClick={handleGetVerifyCode}>
+            {isShowVerifyCode ? (
+              <CountDown time={10} onEnd={handleCountDownEnd} />
+            ) : (
+              '获取验证码'
+            )}
+          </span>
+        </div>
+        <div className={styles.loginBtn} onClick={handleLogin}>
+          登录
+        </div>
+        <div className={styles.otherLogin} onClick={handleOAuthGithub}>
+          使用 Github 登录
+        </div>
+        <div className={styles.loginPrivacy}>
+          注册登录即表示同意
+          <a
+            href="https://moco.imooc.com/privacy.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            隐私政策
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Login;
